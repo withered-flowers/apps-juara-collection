0a6f9f3f059ec56bf551eacfb35139da0336a9b4
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-12-15 19:40:58 +0700

    feat: fallback to use built in chromium

--- src/utils/google-developer/scrapper.ts
+++ src/utils/google-developer/scrapper.ts
@@ -11,18 +11,18 @@ import {
 import type { Badge, EligibleProfile, Profile } from "./defs";
 
 export const analyzeHtml = async (htmlString: string): Promise<Profile> => {
-  const browser = await playwright.launch({
-    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    headless: true,
-    executablePath: await chromium.executablePath(
-      "https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar",
-    ),
-  });
-
   // const browser = await playwright.launch({
   //   args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
   //   headless: true,
+  //   executablePath: await chromium.executablePath(
+  //     "https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar",
+  //   ),
   // });
+
+  const browser = await playwright.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    headless: true,
+  });
 
   const page = await browser.newPage();
 
