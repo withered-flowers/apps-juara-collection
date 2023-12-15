4833d9d124bc8b5f0ca886fe18686aa7c186bdd4
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-12-15 17:59:08 +0700

    feat: change output to server instead of hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
-  adapter: vercel()
+  output: "server",
+  adapter: vercel(),
 });
